876e187b442ec2fe9254d035e83a6285471b8df2
kalane15 <kalane15@yandex.ru>
2025-12-07 23:08:56 +0300

    sad

--- lab1/customRealization/CustomLinearRegression.py
+++ lab1/customRealization/CustomLinearRegression.py
@@ -2,46 +2,31 @@ import numpy as np
 
 
 class CustomLinearRegression:
-    def __init__(self, lr=0.0001, iters=10000000, tol=1e-4):
+    def __init__(self, lr=0.01, iters=1000):
         self.lr = lr
         self.iters = iters
-        self.tol = tol
         self.w = None
         self.b = None
 
     def fit(self, X, y):
         samples, features = X.shape
+
         X = np.array(X)
         y = np.array(y)
         self.w = np.zeros(features)
         self.b = 0
-        prev_loss = float('inf')
-
-        for _ in range(self.iters):
+        while True:
             y_pred = np.dot(X, self.w) + self.b
             error = y_pred - y
 
-            dw = (2 / samples) * np.dot(X.T, error)
-            db = (2 / samples) * np.sum(error)
+            dw = (1 / samples) * np.dot(X.T, error)
+            db = (1 / samples) * np.sum(error)
 
             self.w -= self.lr * dw
             self.b -= self.lr * db
-
-            current_loss = np.mean(error ** 2)
-
-            if np.abs(prev_loss - current_loss) < self.tol:
-                break
-
-            prev_loss = current_loss
 
     def predict(self, X_test):
         return np.dot(X_test, self.w) + self.b
 
     def get_params(self, deep=True):
-        return {'lr': self.lr, 'iters': self.iters, 'tol': self.tol}
-
-    def score(self, X_test, y_test):
-        preds = self.predict(X_test)
-        ss_res = np.sum((y_test - preds) ** 2)
-        ss_tot = np.sum((y_test - np.mean(y_test)) ** 2)
-        return 1 - (ss_res / ss_tot)
+        return {'lr': self.lr, 'iters': self.iters}
